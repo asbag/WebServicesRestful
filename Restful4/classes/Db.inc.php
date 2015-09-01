abd96c7a020f3c52e3fcd2c7193a3fd7e64d3d11
asbag <cirupeto@gmail.com>
2015-09-01 11:11:15 +0200

    New singleton features to Db function

--- Restful4/classes/Db.inc.php
+++ Restful4/classes/Db.inc.php
@@ -11,16 +11,37 @@ class Db {
 	private $password;
 	private $database;
 	private $engine;
-	private $db;
 
+	public static $_singleton;
+	private $dbh;
+	
 	function __construct()  {
 		$this->username = USERNAME;
 		$this->password = PASSWORD;
 		$this->engine = DBENGINE;
 		$this->database = DBNAME;
 		
-		$dsn = $this->engine . ':host=localhost;dbname='  . $this->database . ';charset=utf8';
-		$db = new PDO($dsn, $this->username, $this->password);
+		// set error level to warnings
+		$this->dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
 		
 	}
+	
+	private function connect ($host, $user, $pass, $db) {
+		try {
+			$dsn = $this->engine . ':host=localhost;dbname='  . $this->database . ';charset=utf8';
+			$this->dbh = new PDO($dsn, $this->username, $this->password);
+				
+		} catch (PDOException $e) {
+			return $e->getMessage();
+		}
+	}
+	
+	public function getInstance() {
+		if (!isset(self::$_singleton)) {
+			self::$_singleton = new Db();
+		}
+		return self::$_singleton;
+	}
+	
+	
 }
